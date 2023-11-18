6cee5a18000278840c9cf1d78df5600bbfdbc28b
cagatayyildiz <yildiz.cagatay@hotmail.com>
2023-11-18 16:49:54 +0300

    filter sample

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -6,10 +6,38 @@ function Customers() {
 
     const [customers, setcustomers] = useState(customersData)
 
-    //Country Spain ise o satırı kırmızı yap
+
+    const removeCustomer = (id) => {
+
+        var isDelete = window.confirm("Are you sure?");
+
+        if(isDelete){
+            var filteredCustomers = customers.filter(q => q.id != id);
+            setcustomers(filteredCustomers)
+        }
+      
+    }
+
+    const search = (value) => {
+        
+        var searchData = value.toLowerCase()
+
+        var filteredCustomers = customersData.filter(q => q.companyName.toLowerCase().includes(searchData))
+
+        setcustomers(filteredCustomers)
+
+    }
+    
     return (<>
         <button onClick={() => setcustomers([])}>Empty</button>
         <h4>Length: {customers.length}</h4>
+
+        <hr />
+
+        <div>
+            <label htmlFor="">Search</label>
+            <input type="text" onChange={(e) => search(e.target.value)} />
+        </div>
         <table>
             <thead>
                 <tr>
@@ -18,6 +46,7 @@ function Customers() {
                     <th>Contact Name</th>
                     <th>Contact Title</th>
                     <th>Country</th>
+                    <th>Delete</th>
                 </tr>
             </thead>
             <tbody>
@@ -35,7 +64,9 @@ function Customers() {
                             <td>{item.contactName}</td>
                             <td>{item.contactTitle}</td>
                             <td>{item.address?.country}</td>
-
+                            <td>
+                                <button onClick={() => removeCustomer(item.id)}>Delete</button>
+                            </td>
                         </tr>
 
                     })
